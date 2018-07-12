54142f9d33a3d414888ff98bb7c768db4b88f244
ALienHackerr <boogyal2004@gmail.com>
2018-07-12 18:19:14 +0800

    Add files via upload
    This is the HO3 for the first quarter.Help yourselves.

---
+++ HO3.ino
@@ -0,0 +1,215 @@
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+//this should be in a separate header file;yes,I know
+void setup() {
+  
+  pinMode(4,OUTPUT);
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  
+  tone(4,NOTE_G4,500);
+  Serial.print("Hap-");
+  delay(600);
+  tone(4,NOTE_G4,500);
+  Serial.print("py ");
+  delay(600);
+  tone(4,NOTE_A4,1000);
+  Serial.print("Birth-");
+  delay(1100);
+  tone(4,NOTE_G4,400);
+  Serial.print("day ");
+  delay(500);
+  tone(4,NOTE_C5,500);
+  Serial.print("to ");
+  delay(600);
+  tone(4,NOTE_B4,1200);
+  Serial.print("you ");
+  delay(1450);
+  tone(4,NOTE_G4,500);
+  Serial.print("Hap-");
+  delay(600);
+  tone(4,NOTE_G4,500);
+  Serial.print("py ");
+  delay(600);
+  tone(4,NOTE_A4,1000);
+  Serial.print("Birth-");
+  delay(1100);
+  tone(4,NOTE_G4,400);
+  Serial.print("day ");
+  delay(500);
+  tone(4,NOTE_D5,500);
+  Serial.print("to ");
+  delay(600);
+  tone(4,NOTE_C5,1200);
+  Serial.print("you\n");
+  delay(1450);
+  tone(4,NOTE_G4,500);
+  Serial.print("Hap-");
+  delay(600);
+  tone(4,NOTE_G4,500);
+  Serial.print("py ");
+  delay(600);
+  tone(4,NOTE_G5,1000);
+  Serial.print("Birth-");
+  delay(1100);
+  tone(4,NOTE_E5,1000);
+  Serial.print("day ");
+  delay(1100);
+  tone(4,NOTE_C5,1000);
+  Serial.print("to ");
+  delay(1100);
+  tone(4,NOTE_B4,1000);
+  Serial.print("some-");
+  delay(1100);
+  tone(4,NOTE_A4,1000);
+  Serial.print("one ");
+  delay(1100);
+  tone(4,NOTE_F5,400);
+  Serial.print("Hap-");
+  delay(500);
+  tone(4,NOTE_F5,500);
+  Serial.print("py ");
+  delay(600);
+  tone(4,NOTE_E5,1000);
+  Serial.print("Birth-");
+  delay(1100);
+  tone(4,NOTE_C5,1000);
+  Serial.print("day ");
+  delay(1100);
+  tone(4,NOTE_D5,1000);
+  Serial.print("to ");
+  delay(1100);
+  tone(4,NOTE_C5,1500);
+  Serial.print("youu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);//50
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);//40
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);//30
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);//20
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);//10
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu");
+  delay(30);
+  Serial.print("uu :(               \n\n");
+  delay(1500);
+  
+}

f1ddb79c2e7a14a6afb446f486c52d014deb5d3e
ALienHackerr <boogyal2004@gmail.com>
2018-09-21 10:19:28 +0800

    Proj_0203

---
+++ Proj_0203.ino
@@ -0,0 +1,36 @@
+#include <IRremote.h>
+#include <LiquidCrystal.h>
+
+IRrecv ir(10);
+decode_results res;
+LiquidCrystal lcd(12,11,5,4,3,2);
+
+const int lf=6, lb=7, rf=8, rb=9;
+
+void setup() {
+  
+  lcd.begin(16,2);
+  lcd.setCursor();
+  ir.enableIRIn();
+  pinMode(lf,OUTPUT);
+  pinMode(lb,OUTPUT);
+  pinMode(rf,OUTPUT);
+  pinMode(rb,OUTPUT); 
+  
+}
+
+void loop() {
+  
+  if(ir.decode(&res){
+    
+    unsigned int reso=res.value;
+    
+    switch(reso){
+      
+      case :
+      
+    }
+    
+  }
+  
+}
